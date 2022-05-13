0f24a4397ed240f0c49667744c7454e0641c1b7d
Maciej Słowiak <slowiakmaciej@gmail.com>
2022-05-13 17:29:30 +0200

    Add JPA ManyToMany Relationship example

---
+++ src/main/java/guru/springframework/domain/Category.java
@@ -0,0 +1,44 @@
+package guru.springframework.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import java.util.Set;
+
+@Entity
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String desription;
+
+    @ManyToMany(mappedBy = "categories")
+    private Set<Recipe> recipes;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDesription() {
+        return desription;
+    }
+
+    public void setDesription(String desription) {
+        this.desription = desription;
+    }
+
+    public Set<Recipe> getRecipes() {
+        return recipes;
+    }
+
+    public void setRecipes(Set<Recipe> recipes) {
+        this.recipes = recipes;
+    }
+}
