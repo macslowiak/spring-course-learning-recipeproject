ca27136b3e16043590a5f8e63f71c4518d0dd34a
Maciej Słowiak <maciej.slowiak@cytiva.com>
2022-05-17 10:38:26 +0200

    Add Junit simple test + some fixes

---
+++ src/test/java/guru/springframework/domain/CategoryTest.java
@@ -0,0 +1,33 @@
+package guru.springframework.domain;
+import static org.junit.Assert.assertEquals;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CategoryTest {
+
+    Category category;
+
+    @Before
+    public void setUp(){
+        category = new Category();
+    }
+
+    @Test
+    public void getId() {
+
+        Long idValue = 4L;
+
+        category.setId(idValue);
+
+        assertEquals(idValue, category.getId());
+
+    }
+
+    @Test
+    public void getDescription() {
+    }
+
+    @Test
+    public void getRecipes() {
+    }
+}
