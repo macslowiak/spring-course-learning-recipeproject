e23c03e544db1eb1d900ab943df47a843d91c490
Maciej Słowiak <slowiakmaciej@gmail.com>
2022-05-18 16:51:17 +0200

    Add update recipe functionality (copy of html)

--- src/main/java/guru/springframework/controllers/RecipeController.java
+++ src/main/java/guru/springframework/controllers/RecipeController.java
@@ -4,9 +4,11 @@ import guru.springframework.commands.RecipeCommand;
 import guru.springframework.services.RecipeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -20,7 +22,7 @@ public class RecipeController {
     }
 
 
-    @RequestMapping({"/show/{id}"})
+    @RequestMapping({"/{id}/show"})
     public String getById(Model model, @PathVariable Long id) {
         model.addAttribute("recipe", recipeService.findById(id));
 
@@ -38,6 +40,13 @@ public class RecipeController {
     public String saveOrUpdate(@ModelAttribute RecipeCommand command) {
         RecipeCommand savedRecipeCommand = recipeService.saveRecipeCommand(command);
 
-        return "redirect:/recipe/show/" + savedRecipeCommand.getId();
+        return "redirect:/recipe/" + savedRecipeCommand.getId() + "/show";
+    }
+
+
+    @GetMapping("/{id}/update")
+    public String updateRecipe(@PathVariable String id, Model model){
+        model.addAttribute("recipe", recipeService.findCommandById(Long.valueOf(id)));
+        return  "recipe/recipeform";
     }
 }
