cc431a75caee4c24797a3efbbad982b1ebc7551f
Maciej Słowiak <slowiakmaciej@gmail.com>
2022-05-18 16:14:50 +0200

    Add new recipe functionality

--- src/main/java/guru/springframework/controllers/RecipeController.java
+++ src/main/java/guru/springframework/controllers/RecipeController.java
@@ -1,9 +1,12 @@
 package guru.springframework.controllers;
 
+import guru.springframework.commands.RecipeCommand;
 import guru.springframework.services.RecipeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -18,9 +21,23 @@ public class RecipeController {
 
 
     @RequestMapping({"/show/{id}"})
-    public String getById(Model model,@PathVariable Long id){
-        model.addAttribute("recipe",recipeService.findById(id));
+    public String getById(Model model, @PathVariable Long id) {
+        model.addAttribute("recipe", recipeService.findById(id));
 
         return "recipe/single-recipe";
+    }
+
+    @RequestMapping("/new")
+    public String newRecipe(Model model) {
+        model.addAttribute("recipe", new RecipeCommand());
+
+        return "recipe/recipeform";
+    }
+
+    @PostMapping(name = "recipe")
+    public String saveOrUpdate(@ModelAttribute RecipeCommand command) {
+        RecipeCommand savedRecipeCommand = recipeService.saveRecipeCommand(command);
+
+        return "redirect:/recipe/show/" + savedRecipeCommand.getId();
     }
 }
